4970068b547a6c709faaa4fddadfbe5614e1fa2b
SHEscher <zimon@gmx.com>
2021-03-29 20:47:00 +0200

    temp adapt __main__

--- __main__.py
+++ __main__.py
@@ -7,13 +7,28 @@ Execute __main__.py
 Author: Simon M. Hofmann | <[firstname].[lastname][at]pm.me> | 2021
 """
 
-from PumpkinNet.pumpkinnet import train_simulation_model
+from utils import p2results, cprint, find
+from PumpkinNet.pumpkinnet import train_simulation_model, crop_model_name
 from PumpkinNet.simulation_data import get_pumpkin_set
+from LRP.create_heatmaps import create_relevance_dict, plot_simulation_heatmaps
 
 # TODO temporary:
 def main():
     print(f"\nExecute __main__.py in {__file__}\n")
-    train_simulation_model(pumpkin_set=get_pumpkin_set(n_samples=100), epochs=50)
+    train_simulation_model(pumpkin_set=get_pumpkin_set(n_samples=2000), epochs=80)
+
+    fn = find(fname="final.h5", folder=p2results, typ="file", exclusive=False, fullname=False,
+              abs_path=True, verbose=False)
+
+    for fn in [fn.split("/model/")[-1]]:  # os.listdir(p2results):  # TODO TEMP toggled
+        if "final.h5" in fn:
+            model_name = crop_model_name(model_name=fn)
+            cprint(f"\nCreate heatmaps for {model_name}\n", col="p", fm="bo")
+            rel_obj = create_relevance_dict(model_name=model_name, subset="test", save=True)
+
+            # Plot heatmaps for N random tori
+            plot_simulation_heatmaps(model_name=model_name, n_subjects=20, subset="test", pointers=True,
+                                     true_scale=False)
 
 # TODO Install locally [remove later]
 # python3 setup.py sdist bdist_wheel  # build
